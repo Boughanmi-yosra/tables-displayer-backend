770dcb600d6209b11da65cdae92404eb6a1d1a78
Yosra Boughanmi <yousra.boughanmi@gmail.com>
2025-04-04 16:14:08 +0100

    testing triggers

--- src/main/java/com/yobo/tablesdisplayer/TablesDisplayerBackendApplication.java
+++ src/main/java/com/yobo/tablesdisplayer/TablesDisplayerBackendApplication.java
@@ -2,7 +2,7 @@ package com.yobo.tablesdisplayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Testing triggers
 @SpringBootApplication
 public class TablesDisplayerBackendApplication {
 
